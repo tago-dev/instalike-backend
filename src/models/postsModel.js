76fa03dc59cc3d754d3fcca7146f34cf6931ac2a
Tago <tiagobettega.tb@gmail.com>
2024-11-22 06:26:17 -0300

    uploads

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -2,8 +2,14 @@ import conectarAoBanco from "../config/dbconfig.js";
 
 const conn = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     const db = conn.db("imersao-instabytes");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conn.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 }
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImage } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
     app.use(express.json());
-
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImage);
 }
 
 export default routes;
